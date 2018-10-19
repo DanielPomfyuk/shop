87e1b7a0d5f18837958f76f809cfd30d03b7d296
DanielPomfyuk <danyapomfyuk99@gmail.com>
2018-10-19 12:40:12 +0300

    shop_two

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,13 @@ class ProductsController < ApplicationController
   	@post = Post.new
   end
   def create
- 	 @post = Post.new(post_params(:post)) 
-  if @post.save
-  	redirect_to root_path
-  end
+ 	 @post = Post.new(post_params) 
+	  if @post.save
+	  	redirect_to root_path
+	  end
   end
   private 
-  def post_params
-  params.require(:post).permit(:shopname,:title,:description)
-end
+  	def post_params
+  		params.require(:post).permit(:shopname,:title,:description)
+	end
 end
